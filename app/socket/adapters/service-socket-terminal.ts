1f4d4d5ebe64a0a6984864c8155ceaa7f25b83ef
z-yuchangjun <z-yuchangjun@sharingmail.cn>
2026-04-25 12:45:34 +0800

    del session param

--- app/socket/adapters/service-socket-terminal.ts
+++ app/socket/adapters/service-socket-terminal.ts
@@ -151,6 +151,7 @@ export class ServiceSocketTerminal {
     if (typeof cmd === 'string' && cmd !== '') {
       stream.write(`${cmd}` + '\n')
       this.context.debug('Sent initial command from URL:', cmd)
+        delete req.session!['initialCommand']
     }
   }
 
@@ -160,6 +161,7 @@ export class ServiceSocketTerminal {
         if (typeof shared === 'string' && shared !== '') {
             stream.write(`tmux attach -t ${shared}` + '\n')
             this.context.debug('Sent shared tmux from param:', shared)
+            delete req.session!['initialSharedTmux']
         }
     }
 
@@ -169,6 +171,7 @@ export class ServiceSocketTerminal {
         if (typeof shared === 'string' && shared !== '') {
             stream.write(`screen -x ${shared}` + '\n')
             this.context.debug('Sent shared screen from param:', shared)
+            delete req.session!['initialSharedScreen']
         }
     }
 
